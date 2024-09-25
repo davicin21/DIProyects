2fca804e57abc5225dbdfa8741cc4f341ed654d2
David Alvarez <davidao01@educastur.es>
2024-09-25 10:42:43 +0200

    Agregar archivos de proyecto.

---
+++ PagesDemo/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace PagesDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new AppShell();
+            //MainPage = new MainPage();
+            //MainPage = new ContentPageDemo();
+            MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
---
+++ PagesDemo/ContentPageDemo.xaml.cs
@@ -0,0 +1,18 @@
+namespace PagesDemo;
+
+public partial class ContentPageDemo : ContentPage
+{
+	public ContentPageDemo()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new MainPage());
+    }
+    private void Button_Clicked2(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+}
